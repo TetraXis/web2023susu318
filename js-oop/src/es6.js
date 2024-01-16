78fa60ab3623ebc105caa59c2cd779efa6d5f329
TetraXis <107315342+TetraXis@users.noreply.github.com>
2024-01-16 19:00:07 +0500

    ball3

---
+++ js-oop/src/es6.js
@@ -0,0 +1,70 @@
+"use strict";
+// в данных задачах нужно использовать возможности es6
+// ко всем заданиям можно дописать свои тесты в файле es6.spec.js
+// Можно менять параметры функций (например сделать им значения по умолчанию)
+
+// Напишите функцию, которая принимает ФИО пользователя и возвращает
+// строку формата Имя Фамилия
+function fioToName(fio) {
+    fio = fio.split(" ");
+    return fio[1] + " " + fio[0];
+}
+
+// преобразуйте массив чисел так, чтобы в нем остались только
+// уникальные элементы
+// присмотритесь к коллекции "Set"
+function filterUnique(array) {
+    const set = new Set();
+    for (let i = 0; i < array.length; i++) {
+        set.add(array[i]);
+    }
+    return Array.from(set);
+}
+
+// Задача: разница зарплат
+// в функцию приходит массив из n зарплат сотрудников фирмы
+// ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
+// сотрудника превышает зарплату самого низкооплачиваемого
+// присмотритесь к методу .reduce
+function calculateSalaryDifference(array) {
+    if (array === undefined || array.length == 0) {
+        return false;
+    }
+    function getMin(acc, item, index, arr) {
+        const min = Math.min(acc, item);
+        return min;
+    }
+    function getMax(acc, item, index, arr) {
+        const max = Math.max(acc, item);
+        return max;
+    }
+    return array.reduce(getMax, -1000) / array.reduce(getMin, 1000);
+}
+
+// Реализуйте класс "словарь слов" (как толковый словарь)
+// класс должен быть безопасным и работать только со словами
+// присмотритесь к коллекции "Map"
+// Словарь - (string, string), и все это не null и не undefined
+// * покройте класс тестами
+class Dictionary {
+    constructor() {
+        this.map = new Map();
+    }
+    set(str1, str2) {
+        if (typeof str1 !== 'string' || typeof str2 !== 'string' || str1 === null || str2 === null || str1 === "" || str2 === "" || str1 === undefined || str2 === undefined || this.get(str1) === str2) {
+            return false;
+        }
+        this.map.set(str1, str2);
+        return true;
+    }
+    get(str) {
+        return this.map.get(str);
+    }
+}
+
+module.exports = {
+    fioToName,
+    filterUnique,
+    Dictionary,
+    calculateSalaryDifference
+};
---
+++ js-oop/src/oop.js
@@ -0,0 +1,81 @@
+/**
+ * Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
+ * Если координаты не переданы - 0,0; Аналогично если только 1 координата.
+ * Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
+ */
+class Point {
+    constructor(x, y) {
+        this.x = 0;
+        this.y = 0;
+        if (x !== undefined) {
+            this.x = x;
+        }
+        if (y !== undefined) {
+            this.y = y;
+        }
+    }
+    getR() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+}
+
+/**
+ * Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
+ * который будет наследоваться от точки в двумерном пространстве.
+ * Реализовать статический метод, который возвращает расстояние между Point3D.
+ */
+class Point3D extends Point {
+    constructor(x, y, z) {
+        super(x, y);
+        this.z = 0;
+        if (z !== undefined) {
+            this.z = z;
+        }
+    }
+    static vectorLength(a, b) {
+        return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z));
+    }
+}
+
+/**
+ * Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала.
+ * Предусмотреть 2 варианта инициализации - массивом в конструкторе (из него создается очередь) и без параметров.
+ * Со звездочкой: написать тесты методы класса (oop.spec.js)
+ */
+class Queue {
+    constructor(input) {
+        this.elements = {};
+        this.start = 0;
+        this.end = 0;
+        if (input !== undefined) {
+            for (let i = 0; i < input.length; i++) {
+                this.enqueue(input[i]);
+            }
+        }
+    }
+    enqueue(element) {
+        this.elements[this.end] = element;
+        this.end++;
+    }
+    dequeue() {
+        const item = this.elements[this.start];
+        delete this.elements[this.start];
+        this.start++;
+        return item;
+    }
+    peek() {
+        return this.elements[this.start];
+    }
+    get length() {
+        return this.end - this.start;
+    }
+    get isEmpty() {
+        return this.length === 0;
+    }
+}
+
+module.exports = {
+    Point,
+    Point3D,
+    Queue,
+};
